90a6dd5d2ff3a2718912b2b0a17ecd0e19119587
Michael Clark <mc2.dev@proton.me>
2024-03-28 13:52:28 -0500

    added s3 backend for terraform

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,15 @@
 terraform {
-	
+  backend "s3" {
+    bucket = "terracantus-terraform-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
-provider "aws" {}
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "terracantus-terraform-state"
+}
