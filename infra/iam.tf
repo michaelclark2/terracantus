4569da3e59f95636cccf2bb1e763373ca8b5b0ad
Michael Clark <mc2.dev@proton.me>
2024-03-28 13:52:15 -0500

    terraform fmt

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,42 +1,41 @@
 resource "aws_iam_group" "administrators" {
-	name = "Administrators"
-	path = "/"
+  name = "Administrators"
+  path = "/"
 }
 
 data "aws_iam_policy" "admin_access" {
-	name = "AdministratorAccess"
+  name = "AdministratorAccess"
 }
 
 resource "aws_iam_group_policy_attachment" "administrators" {
-	group = aws_iam_group.administrators.name
-	policy_arn = data.aws_iam_policy.admin_access.arn
+  group      = aws_iam_group.administrators.name
+  policy_arn = data.aws_iam_policy.admin_access.arn
 }
 
 resource "aws_iam_user" "admin" {
-	name = "Administrator"
+  name = "Administrator"
 }
 
 resource "aws_iam_user_group_membership" "admin-dev" {
-	user = aws_iam_user.admin.name
-	groups = [aws_iam_group.administrators.name]
+  user   = aws_iam_user.admin.name
+  groups = [aws_iam_group.administrators.name]
 }
 
 resource "aws_iam_access_key" "admin_access_key" {
-	user = aws_iam_user.admin.name
+  user = aws_iam_user.admin.name
 }
 
 resource "aws_iam_user_login_profile" "administrator" {
-	user = aws_iam_user.admin.name
-	password_reset_required = true
+  user = aws_iam_user.admin.name
 }
 
 output "secret" {
-	value = aws_iam_access_key.admin_access_key.secret
-	sensitive = true
+  value     = aws_iam_access_key.admin_access_key.secret
+  sensitive = true
 }
 
 output "password" {
-	value = aws_iam_user_login_profile.administrator.password
-	sensitive = true
+  value     = aws_iam_user_login_profile.administrator.password
+  sensitive = true
 }
 
