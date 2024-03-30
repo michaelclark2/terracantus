c2ae5f0428ec85e69fe4facfcb859f77b8c7504e
Michael Clark <mc2.dev@proton.me>
2024-03-29 19:03:34 -0500

    refactored docker compose and invoke tasks

--- tasks.py
+++ tasks.py
@@ -3,28 +3,28 @@ from invoke import task
 
 @task
 def dev(c):
-    c.run("docker compose -f docker-compose.dev.yml up", pty=True)
+    c.run("docker compose up", pty=True)
 
 
 @task
 def up(c):
-    c.run("docker compose -f docker-compose.dev.yml up -d", pty=True)
+    c.run("docker compose up -d", pty=True)
 
 
 @task
 def down(c):
-    c.run("docker compose -f docker-compose.dev.yml down", pty=True)
+    c.run("docker compose down", pty=True)
 
 
 @task(down)
 def build(c):
-    c.run("docker compose -f docker-compose.dev.yml build", pty=True)
+    c.run("docker compose build", pty=True)
 
 
 @task(up)
 def migrate(c):
     c.run(
-        "docker compose -f docker-compose.dev.yml exec -it web python manage.py migrate",
+        "docker compose exec -it web python manage.py migrate",
         pty=True,
     )
 
@@ -32,7 +32,7 @@ def migrate(c):
 @task(up)
 def makemigrate(c):
     c.run(
-        "docker compose -f docker-compose.dev.yml exec -it web python manage.py makemigrations",
+        "docker compose exec -it web python manage.py makemigrations",
         pty=True,
     )
 
@@ -40,6 +40,6 @@ def makemigrate(c):
 @task(pre=[migrate], post=[down])
 def init(c):
     c.run(
-        "docker compose -f docker-compose.dev.yml exec -it web python manage.py createsuperuser",
+        "docker compose exec -it web python manage.py createsuperuser",
         pty=True,
     )
